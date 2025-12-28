509efa3b7e998d43a4d2741a61fb51ca08af6950
lariAnj <alarisa2005@gmail.com>
2025-12-28 18:18:00 +0300

    fix: access rights to swagger fixed

--- backend/src/main/java/uni/bugtracker/backend/config/SecurityConfig.java
+++ backend/src/main/java/uni/bugtracker/backend/config/SecurityConfig.java
@@ -48,6 +48,13 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml",
+                                "/swagger-resources/**"
+                        ).permitAll()
+                        .requestMatchers(
                                 "/login.html",
                                 "/index.html",
                                 "/",
